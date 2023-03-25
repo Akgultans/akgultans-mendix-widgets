29f651eff9e42f0d6ef57709bc9a7786f9b39dc5
Uraz Akgultan <uraz@akgultan.com>
2023-03-25 07:45:36 +0100

    Initial commit

---
+++ prettier.config.js
@@ -0,0 +1,47 @@
+const base = require("@mendix/pluggable-widgets-tools/configs/prettier.base.json");
+
+module.exports = {
+    ...base,
+    plugins: [require.resolve("@prettier/plugin-xml")],
+    overrides: [
+        {
+            files: ["CHANGELOG.md"],
+            options: {
+                proseWrap: "preserve"
+            }
+        },
+        {
+            files: "package.json",
+            options: {
+                tabWidth: 2
+            }
+        },
+        {
+            files: "package-lock.json",
+            options: {
+                tabWidth: 4,
+                useTabs: false
+            }
+        },
+        {
+            files: "*.md",
+            options: {
+                proseWrap: "preserve"
+            }
+        },
+        {
+            files: "*.xml",
+            options: {
+                printWidth: 500
+            }
+        },
+        {
+            files: ["*.yaml", "*.yml"],
+            options: {
+                tabWidth: 2,
+                // Disable line wrapping in .yml files
+                printWidth: 99999
+            }
+        }
+    ]
+};
